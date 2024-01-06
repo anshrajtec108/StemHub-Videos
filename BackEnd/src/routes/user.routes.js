6b695da68d2d5a2e1b32ffa76d3fab877a7b12bf
Ansh raj <anshraj108.er@gmail.com>
2024-01-06 23:16:49 +0530

    implement getWatchHistory endpoint

--- BackEnd/src/routes/user.routes.js
+++ BackEnd/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUsercoverImage } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middlewares.js"
 import { verifyJWt } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -23,4 +23,12 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWt , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWt,changeCurrentPassword)
+router.route("/current-user").get(verifyJWt,getCurrentUser)
+router.route("/update-account").patch(verifyJWt,updateAccountDetails)
+router.route("/avatar").patch(verifyJWt,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWt,upload.single("coverImage"),updateUsercoverImage)
+router.route("/c/:username").get(verifyJWt,getUserChannelProfile)
+router.route("/history").get(verifyJWt,getWatchHistory)
+
 export default router
