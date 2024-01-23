1ecfb3d6916e2f5a2e7192478046ac2642cd7f1e
Ansh raj <anshraj108.er@gmail.com>
2024-01-23 20:25:32 +0530

    Compile the tweet function

--- BackEnd/src/controllers/tweet.controller.js
+++ BackEnd/src/controllers/tweet.controller.js
@@ -6,19 +6,61 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
+   const {content}=req.body;
+   if(!content){
+    throw new ApiError(400,"the content is required")
+   }
+  const tweet= await Tweet.create({
+    content,
+    owner:req.user?._id
+   })
+   if(!tweet){
+    throw new ApiError(500,"the tweet is not created ")
+   }
+   return res.status(200)
+    .json(new ApiResponse(200,tweet,"succesfully tweet is created"))
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
+    const userId = new mongoose.Types.ObjectId(req.params.userId);
+    if(!userId){
+        throw new ApiError(400,"the userID not present")
+    }
+    const tweets=await Tweet.find({owner:userId}).exec()
+    if(!tweets){
+        throw new ApiError(400,"the user not have any tweets")
+    }
+    return res.status(200)
+    .json(new ApiResponse(200,tweets,"success "))
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    const {content}=req.body;
+   const tweetId=new mongoose.Types.ObjectId(req.params.tweetId)
+   if(!tweetId){
+    throw new ApiError(400,"the tweet ID error or not existe") 
+   }
+   const updatedTweet= await Tweet.findByIdAndUpdate( 
+    tweetId,
+    {
+        $set:{
+            content,
+        }
+    },
+    {new :true})
+
+if(!updateTweet){
+    throw new ApiError(500,"the tweet is not updated ")
+}
+ return res.status(200)
+ .json(new ApiResponse(200,updatedTweet,"successfully updated the tweeet "))
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    const tweetId=new mongoose.Types.ObjectId(req.params.tweetId);
+    const deleteTweet=await Tweet.findByIdAndDelete(tweetId)
+    res.status(200)
+    .json(new ApiResponse(200,deleteTweet,"succesfully the Tweet is deleted"))
 })
 
 export {
