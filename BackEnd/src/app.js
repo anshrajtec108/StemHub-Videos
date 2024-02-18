a1d476bad17a6487c189f4fc5799a748a59a79a9
Ansh raj <anshraj108.er@gmail.com>
2024-02-18 22:44:00 +0530

    feat: Add ON_PalyVideo function with routing
    This commit adds the ON_PalyVideo function, which handles video playback functionality. The function retrieves the videoId from the request body, updates the user's watch history by adding the videoId, and increments the view count of the corresponding video. Additionally, routing for the ON_PalyVideo function is implemented.

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -24,6 +24,7 @@ import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
+import HistortRouter from "./routes/history.routes.js"
 
 //routes declaration
 app.use("/api/v1/healthcheck", healthcheckRouter)
@@ -35,5 +36,6 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/histoty",HistortRouter)
 
 export { app };
---
+++ BackEnd/src/routes/history.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { ON_PalyVideo } from "../controllers/history.controller.js";
+
+
+const router=Router();
+
+router.use(verifyJWT);
+
+router.route('/onPlayViedo').post(ON_PalyVideo)
+
+export default router
