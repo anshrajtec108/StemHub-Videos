18a4703d2c34c376e30ab11fb661ca300aa6d0e5
Ansh raj <anshraj108.er@gmail.com>
2024-05-21 12:13:38 +0530

    deploy change10

--- FrontEnd/ReactApp/src/App.jsx
+++ FrontEnd/ReactApp/src/App.jsx
@@ -9,33 +9,24 @@ import Shorts from './pages/Shorts/Shorts'
 import PlayList from './pages/PlayList/PlayList'
 import Layout from './Layout';
 
-import CardThumbnail from './Components/CardThumbnail/CardThumbnail.jsx';
-import VideoList from './Components/DashBoard.components/VideoList.jsx';
-import ChannelBannner from './Components/DashBoard.components/ChannelBannner.jsx';
+
 import ChannelView from './Components/DashBoard.components/channelView.components.jsx';
 import Login from './Components/Registration.components/Login.jsx';
 import VideoPlayer from './Components/CardThumbnail/VideoPlayer.jsx';
-import Loader from './Components/CardThumbnail/Loader.jsx';
-import SmallCardThumbnail from './Components/CardThumbnail/smallCardThumbnail.jsx';
-import PlaylistLists from './Components/DashBoard.components/PlaylistLists.jsx';
+
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import ProtectedRoute from './services/ProtectedRoute.jsx';
-import CommentList from './Components/Comment/Comment.jsx';
+
 import Search from './Components/HeaderNav.components/Search.jsx';
 import VideoUpload from './Components/CardThumbnail/VideoUpload.jsx';
 import History from './pages/History/History.jsx';
 import RegistrationForm from './Components/Registration.components/RegisterUser.jsx';
 
 
-// import AutoEvent from './Components/testing/AutoEvent.jsx';
-// import LiveComment from './Components/livecomment/LiveComment.jsx';
 
-// const env = import.meta.env.VITE_BASE_URL;
-
-// Now you can use `env` in your component
 
 function App() {
-  // console.log("env", env)
+  
   const router=createBrowserRouter([
     {
       path:'/',
@@ -86,29 +77,6 @@ function App() {
   ])
 
   return (<>
-    {/* <div>{env}</div> */}
-    {/* <Login/> */}
-    {/* <Dashboard/> */}
-    {/* <RouterProvider router={router}>
-      
-    </RouterProvider> */}
-    {/* <AutoEvent/> */}
-    {/* <LiveComment/> */}
-    {/* <Home/> */}
-    {/* <ChannelBannner/> */}
-    {/* <VideoList/> */}
-    {/* <ChannelView/> */}
-    {/* <VideoPlayer/>    */}
-    {/* <SmallCardThumbnail/> */}
-    {/* <PlaylistLists/> */}
-     {/* <Loader/> */}
-    {/* <LiveStream/> */}
-    {/* <RTMPVideoPlayer/> */}
-    {/* <CardThumbnail/> */}
-    {/* <CommentList/> */}
-   
-
-
     <RouterProvider router={router}>
 
     </RouterProvider>
