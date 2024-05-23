3221f5f8b44d340fd153359cbd74b0570784135d
Ansh raj <anshraj108.er@gmail.com>
2024-05-24 04:11:29 +0530

    deploy change21

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -41,8 +41,8 @@ app.use("/api/v1/history", AutoEvent);
 app.use("/api/v1/event", test);
 
 // Catch-all route
-app.use((req, res, next) => {
-    res.status(404).send('hello');
-});
+// app.use((req, res, next) => {
+//     res.status(404).send('hello');
+// });
 
 export default app;
